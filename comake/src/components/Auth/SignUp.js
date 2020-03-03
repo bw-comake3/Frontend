a73eb507f6f744532338dac35ed64fa8eef930b5
1devhall <devoughnh@gmail.com>
2020-03-03 15:47:26 -0500

    working on login+signup

--- comake/src/components/Auth/SignUp.js
+++ comake/src/components/Auth/SignUp.js
@@ -1,7 +1,36 @@
 import React from 'react'
 import axios from "axios";
 import * as Yup from 'yup';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+ 
 import useInput from "../../hooks/input"
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', 
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 export default function SignUp({ history, values, errors, touched}) {
   //history.push('/signIn')
@@ -21,26 +50,69 @@ export default function SignUp({ history, values, errors, touched}) {
       
   })
   return (
-      <div>
-          <form onSubmit={ handleSubmit }>
-             <input 
-             type='text'
-             name="username"
-             placeholder="username"
-             onChange={e => handleUsername(e.target.value)} 
-             />
-              {/* {touched.username && errors.username && <p className ="errors">* {errors.username}</p>} */}
-             
-             <input 
-             type='password'
-             name="password"
-             placeholder="password"
-             onChange={e => handlePassword(e.target.value)} 
-             />
-             {/* {touched.password && errors.password && <p className = 'errors'>* {errors.password}</p>} */}
-             <button type ='submit'>Sign Up</button>
-          </form>
-      </div>
+    <div>
+    <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign Up
+      </Typography>
+      <form className={classes.form} onSubmit={handleSubmit} >
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+          onChange={e => handleUsername(e.target.value)}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange={e => handlePassword(e.target.value)}
+        />
+              
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign Up!
+        </Button>
+      </form>
+    </div>
+    <Box mt={8}>
+     
+    </Box>
+  </Container>
+  </div>
   )
 }
 
