315052a14ab66a20c47a22a77a169199f36f8903
darkwolfxj <hesitation.dotdot@gmail.com>
2020-03-05 20:00:55 -0500

    validation completed for add issue form

---
+++ comake/src/components/ErrorMessagesAddIssue.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ErrorMessagesAddIssue({ errors }) {
+    return (
+        <>
+          {
+              (errors.name !== undefined || errors.desc !== undefined || errors.city || errors.zip)&&(
+                    <>
+                        <div className="redText">
+                            <div>{ (errors.name !== undefined) ? (Object.values(errors.name)[0]) : (null) }</div>    
+                            <div>{ (errors.desc !== undefined) ? (Object.values(errors.desc)[0]) : (null) }</div>    
+                            <div>{ (errors.city !== undefined) ? (Object.values(errors.city)[0]) : (null) }</div>    
+                            <div>{ (errors.zip !== undefined) ? (Object.values(errors.zip)[0]) : (null) }</div>    
+                        </div>
+                    </>
+              )
+          }  
+        </>
+    )
+}
--- comake/src/components/ProtectedRoutes/ProtectedRouteAddIssue.js
+++ comake/src/components/ProtectedRoutes/ProtectedRouteAddIssue.js
@@ -1,29 +1,31 @@
-import React,{useState} from 'react'
+import React,{ useState, useEffect } from "react"
 import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
 import { addIssue } from "../../actions";
 import useInput from "../../hooks/input";
+import ErrorMessagesAddIssue from "../ErrorMessagesAddIssue";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const useStyles = makeStyles(theme => ({
     root: {
-      '& > *': {
+      "& > *": {
         margin: theme.spacing(1),
       },
     },
     nava: {
-        height: '150px',
+        height: "150px",
 
     },
     navBar: {
-        backgroundColor: '#17202A',
-        color: '#E5E7E9',
-        marginBottom: '100px',
-        height: '100%'
+        backgroundColor: "#17202A",
+        color: "#E5E7E9",
+        marginBottom: "100px",
+        height: "100%"
     },
   }));
 
@@ -40,15 +42,37 @@ const ProtectedRouteAddIssue = ({ history, addIssue }) => {
     const toggleNavbar = () => setCollapsed(!collapsed);
 
     const handleAddIssue = (e) => {
-        e.preventDefault()
+        // e.preventDefault()
         addIssue({ issue: name, description: desc, city, zip })
         setTimeout(() => history.push("/dashboard"), 500) 
     }
+    const validationSchema = yup.object().shape({
+        name: yup
+        .string()
+        .min(10, "Issue name must be a minimum of 10 characters")
+        .max(30, "Issue name cannot exceed 30 characters"),
+        desc: yup
+        .string()
+        .min(30, "Description must be a minimum of 30 characters")
+        .max(190, "Description cannot exceed 190 characters"),
+        city: yup
+        .string()
+        .min(3, "City must be a minimum of 3 characters")
+        .max(15, "City cannot exceed 15 characters"),
+        zip: yup
+        .number()
+        .min(5, "Zip code must be a minimum of 5 characters")
+        .max(99999, "Zip code cannot exceed 5 characters")
+    })
+    const { register, errors, handleSubmit } = useForm({ validationSchema: validationSchema })
+    useEffect(() => {
+        console.log(errors)
+    } ,[errors])
 return (
     <form>
         <div className={classes.nava}>
             <Navbar className={classes.navBar} dark>
-                <NavbarBrand href="/" fontWeight='bold' className="mr-auto">Comake</NavbarBrand>
+                <NavbarBrand href="/" fontWeight="bold" className="mr-auto">Comake</NavbarBrand>
        <NavbarToggler backgroundColor="white" onClick={toggleNavbar} className="mr-2" />
             <Collapse isOpen={!collapsed}>
                 <Nav  className={classes.navbar} >
@@ -58,12 +82,13 @@ return (
          </Navbar>
          </div>
         
-        <form onSubmit={ handleAddIssue }>
+        <form id="form" onSubmit={ handleSubmit(handleAddIssue) }>
             <p>Be sure to fill out all fields, if adding an issue.</p>
-            <input required name="name" type="text" placeholder="Issue" onChange={ e => handleName(e.target.value) }  />
-            <textarea required name="desc" type="text" placeholder="Description" onChange={ e => handleDesc(e.target.value) } />
-            <input required name="city" type="text" placeholder="City" onChange={ e => handleCity(e.target.value) } />
-            <input required name="zip" type="text" placeholder="Zip Code" onChange={ e => handleZip(e.target.value) } /> 
+            <input required name="name" type="text" ref={ register } placeholder="Issue" onChange={ e => handleName(e.target.value) }  />
+            <textarea required name="desc" type="text" ref={ register } placeholder="Description" onChange={ e => handleDesc(e.target.value) } />
+            <input required name="city" type="text" ref={ register } placeholder="City" onChange={ e => handleCity(e.target.value) } />
+            <input required name="zip" type="text" ref={ register } placeholder="Zip Code" onChange={ e => handleZip(e.target.value) } /> 
+            <ErrorMessagesAddIssue errors={ errors }/>
             <Button variant="contained" color="primary" type="submit">Add Issue</Button>
         </form>
     </form>
